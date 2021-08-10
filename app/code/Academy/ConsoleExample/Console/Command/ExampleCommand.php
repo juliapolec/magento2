20ea7f1c05a2192f2f8fa2394ae16d3dad614f38
juliapolec <julia.polec@solteq.com>
2021-08-10 14:38:37 +0200

    test

--- app/code/Academy/ConsoleExample/Console/Command/ExampleCommand.php
+++ app/code/Academy/ConsoleExample/Console/Command/ExampleCommand.php
@@ -6,26 +6,81 @@
     use Symfony\Component\Console\Command\Command;
     use Symfony\Component\Console\Input\InputArgument;
     use Symfony\Component\Console\Input\InputInterface;
+    use Symfony\Component\Console\Input\InputOption;
     use Symfony\Component\Console\Output\OutputInterface;
+    use Magento\Framework\Filesystem\Driver\File;
+    use Magento\Framework\Module\Dir\Reader;
 
 
     class ExampleCommand extends Command
-{
-
-    protected function configure()
-
     {
+        const FILE = 'file';
+//                    public const FILE_NAME_ARGUMENT = 'file';
+//                    public const IMPORT_DIR = '/import/';
+        protected function configure()
 
-        parent::configure();
-        $this->setName('academy:example:command');
-        $this->setDescription('Academy training console example.');
-        //$this->addArgument('product_id', InputArgument::REQUIRED, 'Product id');
-        $this->addArgument('product_ids', InputArgument::REQUIRED| InputArgument::IS_ARRAY, 'Product ids');
+        {
+
+            parent::configure();
+            $this->setName('academy:example:command');
+            $this->setDescription('Academy training console example.');
+            //$this->addArgument('product_id', InputArgument::REQUIRED, 'Product id');
+//        $this->addArgument('product_ids', InputArgument::REQUIRED| InputArgument::IS_ARRAY, 'Product ids');
+            $this->addOption(
+                self::FILE,
+                null,
+                InputOption::VALUE_REQUIRED,
+                'file'
+            );
+            parent::configure();
+        }
+
+
+        protected $reader;
+        protected $file;
+        protected $fileName = null;
+
+
+        public function __construct(
+
+            Reader        $reader,
+            File          $file
+
+        )
+
+        {
+            parent::__construct();
+            $this->reader = $reader;
+            $this->file = $file;
+
+        }
+
+
+        /**
+         * @throws \Magento\Framework\Exception\FileSystemException
+         */
+        protected function execute(InputInterface $input, OutputInterface $output)
+        {
+                $fileName = $input->getOption(self::FILE);
+
+                $PathDirectory = $this->reader->getModuleDir(
+                    \Magento\Framework\Module\Dir::MODULE_ETC_DIR,
+                    'Academy_TurboModule');
+                $FullPathDirectory = $PathDirectory. '/'. $fileName;
+
+                $output->writeln( $fileName );
+                $output->writeln( $FullPathDirectory );
+//                $output->writeln($this->file->fileGetContents($PathDirectory));
+
+                if($this->file->isExists($FullPathDirectory)){
+                    $output->writeln("exist ". $fileName);
+                    $output->writeln($this->file->fileGetContents($FullPathDirectory));
+                } else{
+                    $output->writeln("not found");
+                }
+
+        }
     }
-
-
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
 //        formatted text
 //        $output->writeln('<info>Hello world!</info>');
 //        $output->writeln('<comment>Hello world!</comment>');
@@ -38,7 +93,8 @@
 //        $output->writeln($productid);
 
 
-        $productids = $input->getArgument('product_ids');
-        $output->writeln($productids);
-    }
-}
+
+//        $productids = $input->getArgument('product_ids');
+//        $output->writeln($productids);
+//    }
+//}
