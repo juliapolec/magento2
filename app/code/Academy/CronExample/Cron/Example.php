189f9773f936f6987ec6ae2416e15e4fe8fe98cd
juliapolec <julia.polec@solteq.com>
2021-09-15 14:09:09 +0200

    ip

--- app/code/Academy/CronExample/Cron/Example.php
+++ app/code/Academy/CronExample/Cron/Example.php
@@ -4,19 +4,44 @@
     namespace Academy\CronExample\Cron;
 
 use Psr\Log\LoggerInterface;
+use Academy\IpTask\Api\Data\IpAddressInterface;
+use Academy\IpTask\Api\IpAddressRepositoryInterface;
+use Magento\Framework\HTTP\PhpEnvironment\RemoteAddress;
+
 
     class Example
 {
-    protected $logger;
+        protected $logger;
+        private IpAddressRepositoryInterface $ipAddressRepository;
+        private IpAddressInterface $ipAddress;
+        private \Magento\Store\Model\StoreManagerInterface $storeManager;
+        private RemoteAddress $remote;
 
-    public function __construct(LoggerInterface $logger)
+
+        public function __construct(
+        LoggerInterface $logger,
+        IpAddressRepositoryInterface $ipAddressRepository,
+        IpAddressInterface $ipAddress,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        RemoteAddress $remote )
+
     {
         $this->logger = $logger;
+        $this->ipAddressRepository = $ipAddressRepository;
+        $this->ipAddress = $ipAddress;
+        $this->storeManager = $storeManager;
+        $this->remote = $remote;
+
     }
 
     public function execute()
     {
-        $this->logger->info('cron example works');
+        $ipAddress = file_get_contents('https://ipinfo.io/ip');
 
+            $this->logger->info('cron example works');
+
+        $this->ipAddress->setCurrentIpAdddress($ipAddress);
+        $this->ipAddressRepository->save($this->ipAddress);
     }
 }
+
